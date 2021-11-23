1bb8c7e0c2303c3bb4b62c1ed89327609821a99a
harishanveerangan13 <harishanveerangan@gmail.com>
2021-11-22 19:27:19 -0600

    till  if conditon

--- main.py
+++ main.py
@@ -13,3 +13,86 @@
 #
 # a=int(input("enter the string"))
 # print(type(a))
+
+#data types
+# int ,char,float,string
+#10,'a',1.2345,"abcf"
+
+#keywords
+# for,while,print
+#variables
+# a=10
+# ###should be
+# aA
+# AA
+#
+# Aa_
+# Aa1
+# A
+# a
+# #not should
+# A b
+# A@
+# A$
+
+
+#operators
+# arthimetic operators ->+,-,*,/,%
+# print(11/2)
+# print(11//2)
+# print(11%2)
+
+
+#relational operators -> <,>,<=,>=,==,!=
+#logical operators --> &,|,! and,or,not
+# & and
+# T F  F
+# F F F
+# F T F
+# T T T
+# #| or
+# T F T
+# F T T
+# F F F
+# T T T
+
+#
+#not !
+# T F
+# F T
+
+# Binary operators
+# xand,xor ,and ,or ,not
+
+
+#membership operator
+# in ,not in
+# print("pn" not in "python")
+
+
+#if condition
+# a=10
+# if(a<=5):
+#     print("a is less or equal to five")
+# else:
+#     print("a is greathern five")
+
+# a=100
+# if(a<=5):#false
+#     print("a is less or equal to five")
+# elif (a == 10):#False
+#     print("a equal to 10")
+# else:
+#     print("a is greathern five")
+
+# nested if
+# a=10
+# b=20
+# if(a <=5):#False
+#     if(b >= 20):#
+#         print(a)
+
+#Task #
+#check whether the given year is  leap year use input statment
+#find the largest of three numbers use input statement
+
