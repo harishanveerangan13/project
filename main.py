7146cc439f0fb707f6757a2618118d227cf21e87
harishanveerangan13 <harishanveerangan@gmail.com>
2021-11-22 18:54:37 -0600

    my first program

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# print("Harishan")
+# a=10
+# b=20
+# print("a value {0} b value {1}".format(a,b))
+
+#comment -ctrl+/
+#uncomment -ctrl+/
+
+#input
+# a=input("enter the string")
+# print(type(a))
+#
+# a=int(input("enter the string"))
+# print(type(a))
